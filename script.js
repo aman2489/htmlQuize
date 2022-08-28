9f2367c36eb6c5db183712dbb3abb72656b73098
aman2489 <aman1100vishnoi@gmail.com>
2022-08-28 16:56:07 +0530

    updaated

--- script.js
+++ script.js
@@ -1,116 +1,111 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 
-const questionContainerElement = document.getElementById('question-container')
-const qusetionElement = document.getElementById('question')
-const answerButtonElement = document.getElementById('answer-button')
+const questionContainerElement = document.getElementById("question-container");
+const qusetionElement = document.getElementById("question");
+const answerButtonElement = document.getElementById("answer-button");
 
-let shuffledQuestions,currentQuestionIndex;
-let quizScore=0;
+let shuffledQuestions, currentQuestionIndex;
+let quizScore = 0;
 
-startButton.addEventListener('click',startGame)
-nextButton.addEventListener('click',() =>{
-    currentQuestionIndex++
-    setnextQuestion()
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setnextQuestion()
 })
 
-function startGame(){
-    startButton.classList.add('hide')
-    shuffledQuestions=questions.sort(() =>Math.random() -0.5)
-    currentQuestionIndex=0;
-    questionContainerElement.classList.remove('hide')
-    setnextQuestion()
-    quizScore=0;
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide')
+  setnextQuestion()
+  quizScore = 0
 }
 
-function setnextQuestion(){
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+function setnextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question){
-    qusetionElement.innerText= question.question;
-    question.answers.forEach((answer) =>{
-        const button =document.createElement('button')
-        button.innerText=answer.text;
-        button.classList.add('btn')
-        if(answer.correct){
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click',selectAnswer)
-        answerButtonElement.appendChild(button)
-    })
-}
-
-function resetState(){
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while(answerButtonElement.firstChild){
-        answerButtonElement.removeChild(answerButtonElement.firstChild)
+function showQuestion(question) {
+  qusetionElement.innerText = question.question
+  question.answers.forEach((answer) => {
+    const button = document.createElement('button')
+    button.innerText = answer.text;
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
+    button.addEventListener('click', selectAnswer)
+    answerButtonElement.appendChild(button);
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonElement.firstChild) {
+    answerButtonElement.removeChild(answerButtonElement.firstChild)
+  }
 }
 
 function selectAnswer(e){
-    const selectedButton=e.target
-    const correct = selectedButton.dataset.correct
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
 
-    setStatusClass(document.body,correct)
-    Array.from(answerButtonElement.children).forEach((button)=>{
-        setStatusClass(button,button.dataset.correct)
-    })
-    if(shuffledQuestions.lenght > currentQuestionIndex + 1){
-        nextButton.classList.remove("hide")
-    }
-    else{
-        startButton.innerText="restart"
-        startButton.classList.remove("hide")
-    }
-    if(selectedButton.dataset = correct){
-        quizScore++
-    }
-    document.getElementById('right-answers').innerText=quizScore
+  setStatusClass(document.body,correct)
+  Array.from(answerButtonElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.lenght > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+  } else {
+    startButton.innerText = "restart"
+    startButton.classList.remove("hide")
+  }
+  if (selectedButton.dataset = correct) {
+    quizScore++;
+  }
+  document.getElementById('right-answers').innerText = quizScore
 }
 
-function setStatusClass(element,correct){
-    clearStatusClass(element)
-    if(correct){
-        element.classList.add("correct")
-    }
-    else{
-        element.classList.add("wrong")
-    }
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
 }
 
-function clearStatusClass(element){
-    element.classList.remove   ('correct')
-    element.classList.remove   ('wrong')
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 
 const questions = [
-    {
-        question: 'Which one of these is a javaScript framework?',
-        answers :[
-            { text: 'Python', correct: false},
-            { text: 'Django', correct: false},
-            { text: 'React', correct: true},
-            { text: 'Eclipse', correct: false}
-        ],
-    },
-    {
-        question: 'Who is the prime minister of India',
-        answers :[
-            { text: 'Narendra Modi', correct: true},
-            { text: 'Rahul Gandi', correct: false},
-        ],
-    },
-    {
-        question: 'What is 4x3?',
-        answers :[
-            {text: '6', correct: false},
-            {text: '12', correct: true}
-        ]
-        
-    }
-]
-
-
+  {
+    question: "Which one of these is a javaScript framework?",
+    answers: [
+      { text: "Python", correct: false },
+      { text: "Django", correct: false },
+      { text: "React", correct: true },
+      { text: "Eclipse", correct: false },
+    ],
+  },
+  {
+    question: "Who is the prime minister of India",
+    answers: [
+      { text: "Narendra Modi", correct: true },
+      { text: "Rahul Gandi", correct: false },
+    ],
+  },
+  {
+    question: "What is 4x3?",
+    answers: [
+      { text: "6", correct: false },
+      { text: "12", correct: true },
+    ]
+  }
+];
